7642d494090924a278cd7586f918d2faaab5d7d1
jackshang <jackshang94@gmail.com>
2019-03-18 22:46:52 +0800

    push1

--- src/CreationalPatterns/FactoryPattern/test.java
+++ src/CreationalPatterns/FactoryPattern/test.java
@@ -1,5 +1,10 @@
 package CreationalPatterns.FactoryPattern;
 
 public class test {
-  int test;
+	int test;
+
+	public static void main(String[] args) {
+		System.out.print("Hello word");
+		
+	}
 }
